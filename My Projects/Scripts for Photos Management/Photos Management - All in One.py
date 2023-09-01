f833e83c2e94b0203a7c9c0465f5fa6a1323362f
Anant <anant13u@gmail.com>
2023-09-01 23:59:03 +0530

    Changed the script to add (2) in front of duplicate file's name while moving to the dedicated folder.

--- My Projects/Scripts for Photos Management/Photos Management - All in One.py
+++ My Projects/Scripts for Photos Management/Photos Management - All in One.py
@@ -26,83 +26,106 @@ entries = os.listdir(base_path)
 
 
 for file in entries:
-    curr_file_path = os.path.join(base_path,file)
+    curr_file_path = os.path.join(base_path,file) # curr_file_path is C:\Users\Anant\Documents\Test Folder\New folder\IMG_1109.MOV
     if Path(curr_file_path).is_file():
-        # print(full_file_path)
-        # print(Path(full_file_path).suffix.lower())
-        # print(Path(full_file_path).is_file())
-        # print(os.path.getsize(full_file_path)/(1024*1024))
-        file_size = os.path.getsize(curr_file_path)/(1024*1024)
-        file_ext = Path(curr_file_path).suffix.lower()
-        if file_ext=='.mov' and file_size<7:
+        file_size = os.path.getsize(curr_file_path)/(1024*1024) # file_size is 5.0469865798950195
+        file_ext = Path(curr_file_path).suffix # file_ext is .MOV
+        filename = file.split('.')[0] # filename is IMG_1109
+
+        if file_ext.lower()=='.mov' and file_size<7:
         # if file_ext=='.mov' and 1<file_size<7:
-            # print(f'{file} could be a Live Photo.')
             if not os.path.exists(live_folder_path):
                 os.mkdir(live_folder_path)
-            new_file_path=os.path.join(live_folder_path,file)
-            os.rename(curr_file_path,new_file_path)
-            with open(os.path.join(live_folder_path,'Probable Live Photos.txt'),'a+') as curr_log:
-                curr_log.write(f'\nMoved file - "{file}". Size: {round(file_size,2)} MB.')
+            new_file_path=os.path.join(live_folder_path,file) # new_file_path is C:\Users\Anant\Documents\Test Folder\New folder\Probable Live Photos (New folder)\IMG_1109.MOV
+            try:
+                os.rename(curr_file_path,new_file_path)
+                with open(os.path.join(live_folder_path,'Probable Live Photos.txt'),'a+') as curr_log:
+                    curr_log.write(f'\nMoved file - "{file}". Size: {round(file_size,2)} MB.')
+            except Exception as e:
+                new_file_path=os.path.join(live_folder_path,f'{filename} (2){file_ext}') # new_file_path is C:\Users\Anant\Documents\Test Folder\New folder\Probable Live Photos (New folder)\IMG_1109 (2).MOV
+                os.rename(curr_file_path,new_file_path)
+                with open(os.path.join(live_folder_path,'Probable Live Photos.txt'),'a+') as curr_log:
+                    curr_log.write(f'\nMoved file - "{filename} (2){file_ext}". Size: {round(file_size,2)} MB.')
+                print(f"An error occurred: {e}")
 
-        elif file_ext=='.aae':
+        elif file_ext.lower()=='.aae':
             if not os.path.exists(misc_folder_path):
                 os.mkdir(misc_folder_path)
             if not os.path.exists(aae_folder_path):
                 os.mkdir(aae_folder_path)
             new_file_path=os.path.join(aae_folder_path,file)
-            os.rename(curr_file_path,new_file_path)
-            with open(os.path.join(aae_folder_path,'AAE Files.txt'),'a+') as curr_log:
-                curr_log.write(f'\nMoved AAE file - "{file}".')
+            try:
+                os.rename(curr_file_path,new_file_path)
+                with open(os.path.join(aae_folder_path,'AAE Files.txt'),'a+') as curr_log:
+                    curr_log.write(f'Moved AAE file - {file}.\n')
+            except FileExistsError as e:
+                print(f"File '{file}' already exists in the destination. Skipping.")
+            except Exception as e:
+                print(f"An error occurred: {e}")
 
-        elif file_ext=='.png':
+        elif file_ext.lower()=='.png':
         # print(f'{file} is an AAE file.')
             if not os.path.exists(misc_folder_path):
                 os.mkdir(misc_folder_path)
             if not os.path.exists(png_folder_path):
                 os.mkdir(png_folder_path)
             new_file_path=os.path.join(png_folder_path,file)
-            os.rename(curr_file_path,new_file_path)
-            with open(os.path.join(png_folder_path,'PNG Files.txt'),'a+') as curr_log:
-                curr_log.write(f'Moved PNG file - "{file}".\n')
-                
-        elif 0<file_size<1 or file_ext=='.mp4' or file_ext=='.gif':
+            try:
+                os.rename(curr_file_path,new_file_path)
+                with open(os.path.join(png_folder_path,'PNG Files.txt'),'a+') as curr_log:
+                    curr_log.write(f'Moved PNG file - {file}.\n')
+
+            except Exception as e:
+                new_file_path=os.path.join(png_folder_path,f'{filename} (2){file_ext}')
+                os.rename(curr_file_path,new_file_path)
+                with open(os.path.join(png_folder_path,'PNG Files.txt'),'a+') as curr_log:
+                    curr_log.write(f'Moved PNG file - {filename} (2){file_ext}.\n')
+                print(f"An error occurred: {e}")
+
+        elif 0<file_size<1 or file_ext.lower()=='.mp4' or file_ext.lower()=='.gif':
         # elif 0<file_size<1 or (file_ext=='.mp4' and 1<file_size<7):
             # print(f'{file} - file or not status: {Path(curr_file_path).is_file()}')
             if not os.path.exists(misc_folder_path):
                 os.mkdir(misc_folder_path)
             new_file_path=os.path.join(misc_folder_path,file)
-            os.rename(curr_file_path,new_file_path)
+            try:
+                os.rename(curr_file_path,new_file_path)
+            except Exception as e:
+                new_file_path=os.path.join(misc_folder_path,f'{filename} (2){file_ext}')
+                os.rename(curr_file_path,new_file_path)
+                print(f"An error occurred: {e}")
 
-
-for edited_file in entries:
-    edited_file_path = os.path.join(base_path,edited_file)
-    # Check if the entry is a regular file and starts with 'IMG_E'.
-    if Path(edited_file_path).is_file() and edited_file.startswith('IMG_E'):
-        try:
+        elif file.startswith('IMG_E'): # Checking if the entry starts with 'IMG_E'.
+            curr_file_path = os.path.join(base_path,file) # Edited File's path is C:\Users\Anant\Documents\Test Folder\New folder\IMG_E6293.JPG
             # Create the edited folder if it doesn't exist.
             if not os.path.exists(edited_folder_path):
                 os.mkdir(edited_folder_path)
-            # Rename the edited file and move it to the edited folder.
-            edited_file_new_path=os.path.join(edited_folder_path,edited_file)
-            os.rename(edited_file_path,edited_file_new_path)
+            edited_file_new_path=os.path.join(edited_folder_path,file) # Rename the edited file and move it to the edited folder.
+            try:
+                os.rename(curr_file_path,edited_file_new_path)
+            except Exception as e:
+                edited_file_new_path=os.path.join(edited_folder_path,f'{filename} (2){file_ext}')
+                error_list.append(f'{e}\n')
+                print(f"An error occurred: {e}")
 
-            # Create the name of the original file and its path.
-            original_file=edited_file.replace('_E','_')
+            original_file=file.replace('_E','_') # Create the name of the original file and its path.
             original_file_path = os.path.join(base_path,original_file)
             if os.path.exists(original_file_path):
                 # Rename the original file and move it to the edited folder.
                 original_file_new_path=os.path.join(edited_folder_path,original_file)
                 # original_file is os.path.basename(original_file_curr_path).
-                os.rename(original_file_path, original_file_new_path)
+                try:
+                    os.rename(original_file_path, original_file_new_path)
+                except Exception as e:
+                    original_file_new_path=os.path.join(edited_folder_path,f"{original_file.split('.')[0]} (2){file_ext}")
+                    error_list.append(f'{e}\n')
+                    print(f"An error occurred: {e}")
 
                 # Write a log entry for the moved pair of files.
-                moved_pairs_list.append(f'Moved pair of files - {original_file} and {edited_file}.')
+                moved_pairs_list.append(f'Moved pair of files - {original_file} and {file}.')
             else:
-                 moved_edited_only_list.append(f"Moved file - {edited_file}. Original file not found for {edited_file}.")
+                moved_edited_only_list.append(f"Moved file - {file}. Original file not found for {file}.")
 
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            error_list.append(f'{e}\n')
 
 with open(edit_log_path,'a+') as edit_log:
     edit_log.write("\n".join(moved_pairs_list) + "\n\n\n" + "\n".join(moved_edited_only_list) + "\n\n\n" + "\n".join(error_list))
