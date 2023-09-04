4ac0dc39beaf917352fb1b815ed0b3cde2bf87e4
Anant <anant13u@gmail.com>
2023-09-05 02:59:29 +0530

    Created 2 separate logs for listing files and directories which will be written to the final log file in the end.

--- My Projects/Scripts for Photos Management/Check files and folders in another folder.py
+++ My Projects/Scripts for Photos Management/Check files and folders in another folder.py
@@ -9,20 +9,29 @@ base_path = input('Please enter the path where you want to search for Live Photo
 entries = os.listdir(base_path)
 # print(entries)
 
-log = []
+dirs_log = ['Directories:\n']
+files_log = ['\n\n\nFiles:\n']
+log_file = os.path.join(base_path,'Log File.txt')
 
 for entry in entries:
     entry_path = os.path.join(base_path,entry)
     try:
         if Path(entry_path).is_dir():
-            print(f'{entry} is a directory.')
+            dirs_log.append(entry)
+            # print(f'{entry} is a directory.')
         elif Path(entry_path).is_file:
-            print(f'{entry} is a file.')
+            files_log.append(entry)
+            # print(f'{entry} is a file.')
         else:
             print(f'{entry} type is unknown.')
     except Exception as e:
         print(f'Encountered error with {entry}: {e}')
+
+with open(log_file,'w') as log_file:
+    log_file.write('\n'.join(dirs_log) + '\n'.join(files_log))
         
 # /media/anant/Games, Torrents, Other Stuff
 # /media/anant/Games, Torrents, Other Stuff/Torrents/Movies and Shows
+# /home/anant/Downloads
+
 
