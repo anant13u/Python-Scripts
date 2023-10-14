3f7ca48cd7a947c21b68bec613113591561a8d2c
Anant <anant13u@gmail.com>
2023-10-14 23:59:18 +0530

    Added code to open ChatGPT in browser.

--- My Projects/Git Message Prompt Creator.py
+++ My Projects/Git Message Prompt Creator.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 import pyperclip
+import webbrowser
 
 sg.theme('Reddit')
     
@@ -21,6 +22,7 @@ while True:
         # sg.popup(initialCode,currentCode)
         prompt = f'I need you to write a Git commit message (preferably in 3-4 lines max) for the code changes made below:\nPrevious code:\n{initialCode}\n\nCurrent Code:\n{currentCode}' 
         pyperclip.copy(prompt)
+        webbrowser.open('https://chat.openai.com/')
 
 
 # On Linux, install xclip, xsel, or wl-clipboard (for "wayland" sessions) via package manager. For example, in Debian:
