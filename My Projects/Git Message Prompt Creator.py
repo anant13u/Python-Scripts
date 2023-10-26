2b4e5b1e51c30e7254ec6142919ce536ace101e9
Anant <anant13u@gmail.com>
2023-10-26 18:19:13 +0530

    Added code to wait for 2 seconds and paste in ChatGPT inputbox.

--- My Projects/Git Message Prompt Creator.py
+++ My Projects/Git Message Prompt Creator.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
-
 import PySimpleGUI as sg
 import pyperclip
 import webbrowser
+import time
 
 sg.theme('Reddit')
     
@@ -25,6 +24,8 @@ while True:
         prompt = f'I need you to write a Git commit message (preferably in 3-4 lines max) for the code changes made below:\nPrevious code:\n{initialCode}\n\nCurrent Code:\n{currentCode}' 
         pyperclip.copy(prompt)
         webbrowser.open('https://chat.openai.com/')
+        time.sleep(2)
+        pyperclip.paste
 
 
 # On Linux, install xclip, xsel, or wl-clipboard (for "wayland" sessions) via package manager. For example, in Debian:
