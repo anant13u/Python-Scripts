5046df4361560f55954800f0be056d97e6ea45eb
Anant <anant13u@gmail.com>
2023-11-07 09:59:12 +0530

    Removed commented out code meant for the CLI version.

--- My Projects/ConvertVideoToAudio.py
+++ My Projects/ConvertVideoToAudio.py
@@ -42,14 +42,5 @@ while True:
             print(e)
 
 
-# Insert Local Video File Path 
-# pth =  input('\nPlease enter the path of the video along with the extension which you wish to convert: ')
-# pth = pth.replace('"','')
-# clip = mp.VideoFileClip(pth)
-  
-# newfile = pth.replace('.mp4','.mp3') # Here 'newfile' will the complete path for the newly created audio file.
-
-# Insert Local Audio File Path
-# clip.audio.write_audiofile(newfile)
 
 # "C:/Users/AU/Videos/InfraSound - Starlight   Epic Powerful Hybrid Orchestral Music.mp4"
