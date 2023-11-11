edd5bb8931d51e4e016f6d42916e159abd183e27
Anant <anant13u@gmail.com>
2023-11-11 12:25:58 +0530

    Added a list containing mp4, avi and mkv extensions that will be checked against when a file is selected. If the file's extension does not match any of  these 3 then conversion will not go ahead. Also added another exception that will flag the FileNotFoundError.

--- My Projects/ConvertVideoToAudio.py
+++ My Projects/ConvertVideoToAudio.py
@@ -1,21 +1,26 @@
 # Python code to convert video to audio
 
 import PySimpleGUI as sg
-# from pathlib import Path
+from pathlib import Path
 import os
 import time
 import subprocess
 import moviepy.editor as mp # Before this the moviepy module needs to be installed using PIP
 
+valid_video_extensions = ['.mp4', '.avi', '.mkv']
+
 sg.theme('darkgreen7')
 sg.set_options(font=("Helvetica", 11))
 
+# Define the layout of the GUI
 layout = [  [sg.FileBrowse('Select Video',key='input-video',pad=(30,20)), sg.B('Convert to Audio'), sg.B('Exit',pad=(30,20))],
             [sg.T(key='file_display', pad=10)]  ]
 
+# Create the PySimpleGUI window
 Window = sg.Window('Video -> Audio by AU', layout, grab_anywhere=True, keep_on_top=True)
 
 
+# Main event loop
 while True:
     event, values = Window.read()
     if event in (sg.WIN_CLOSED, 'Exit'):
@@ -23,23 +28,30 @@ while True:
     if event=='Convert to Audio':
         try:
             inputVideo = values['input-video']
+            if Path(inputVideo).suffix.lower() not in valid_video_extensions:
+                sg.popup_error(f'The selected file has {Path(inputVideo).suffix.lower()} extension. \nPlease select a valid video file (e.g., .mp4, .avi, .mkv)', keep_on_top=True)
+                continue
             Window['file_display'].update(inputVideo)
             outputFolder = os.path.dirname(inputVideo)
-            print(f'Output Folder is {outputFolder}')
             print(f'Selected file name is {os.path.basename(inputVideo)}')
             outputAudio = inputVideo.replace('.mp4', '.mp3')
             # print(our_clip)
             our_clip=mp.VideoFileClip(inputVideo)
             our_clip.audio.write_audiofile(outputAudio, codec='mp3')
+            sg.popup('Conversion successful!', f'Audio saved as {outputAudio}')
             if subprocess.os.name == 'nt':  # Check if the platform is Windows
                 subprocess.Popen(f'explorer {outputFolder}')
                 # print(subprocess.Popen(r'explorer outputFolder'))
             else:  # For non-Windows platforms (e.g., Linux, macOS)
                 subprocess.Popen(['xdg-open', outputFolder])
+        except FileNotFoundError as e:
+            sg.popup_error(f'File not found: {e}', keep_on_top=True)
+            print(e)
         except Exception as e:
             sg.popup_error(f'Error: {e}',keep_on_top=True)
             print(e)
 
+ 
 
 # /home/anant/new
 # "C:/Users/AU/Videos/InfraSound - Starlight   Epic Powerful Hybrid Orchestral Music.mp4"
