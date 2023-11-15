8885de3b2a8fdd76da47e62b2f296f06dbae9dc2
Anant <anant13u@gmail.com>
2023-11-15 10:13:21 +0530

    Added f-strings to rest of the lines.

--- For Reference/Calendar and DateTime/Get Current Date.py
+++ For Reference/Calendar and DateTime/Get Current Date.py
@@ -10,11 +10,11 @@ print(f'Date in date/month/year format = {date1}')
 # Date in date/month/year format = 07/05/2022
 
 date2 = curr_date.strftime('%B %d, %Y')
-print('Date in month day, year format =', date2)
+print(f'Date in month day, year format = {date2}')
 # Date in month day, year format = May 07, 2022
 
 date3 = curr_date.strftime('%b-%d-%Y')
-print('Date in month-date-year format =', date3)
+print(f'Date in month-date-year format = {date3}')
 # Date in month-date-year format = May-07-2022
 
 date4 = curr_date.strftime('Month is %m, Date is %d, Year is %Y')
