1c25899981079363619186690f961d984dbbbe09
Anant <anant13u@gmail.com>
2023-12-02 13:09:28 +0530

    Created a script to make the system go into hibernation with one click.

---
+++ My Projects/Hibernate Command.py
@@ -0,0 +1,16 @@
+import os
+import PySimpleGUI as sg
+
+layout = [[sg.T('Do you really want to go into Hibernation, lol')],
+          [sg.Button('Yepp!'), sg.B('Cancel')]]
+
+Window = sg.Window('Hibernate or Not', layout)
+
+while True:
+    event, values = Window.read()
+    if event in (sg.WIN_CLOSED, 'Cancel'):
+        break
+    elif event=='Yepp!':
+        # This will make the system go into hibernate mode.
+        sg.popup_auto_close('Bye',auto_close_duration=2)
+        os.system('shutdown /h')
