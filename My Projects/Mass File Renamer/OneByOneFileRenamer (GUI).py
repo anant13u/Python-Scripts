0b430107e78cee0327c258305c64c2be382a1427
Anant <anant13u@gmail.com>
2023-12-04 21:32:40 +0530

    Added a button to display all the files of currently selected folder in a popup.

--- My Projects/Mass File Renamer/OneByOneFileRenamer (GUI).py
+++ My Projects/Mass File Renamer/OneByOneFileRenamer (GUI).py
@@ -9,20 +9,26 @@ sz= size=(20,2)
 def mains():
     layout = [  [sg.Text('Please select the folder where you want to rename the files',size=(45,2), pad=(20,(30,10))), sg.FolderBrowse(key='-IN-',size=(15,2),pad=((30,20),10))],
                 [sg.Text("Current File's Name:",pad=(20,20)),sg.T('',key='old-filename',pad=((65,0),15))],
-                [sg.Text('Please enter the new name:',pad=(20,20)),sg.Input('',key='new-filename',size=(40,1),pad=(20,15))],
-                [sg.Button('Begin',key='rename-button',size=(10,2),pad=((150,20),20)),sg.T(' '*10),sg.Button('Exit',size=(10,2),pad=((20,70),15))]    ]
+                [sg.Text('Please enter the new name:',pad=(20,20)),sg.Input('',key='new-filename',size=(45,1),pad=(20,15))],
+                [sg.B('Get Files List',size=(13,2),pad=(50,20)),sg.B('Begin',key='rename-button',size=(10,2),pad=((30,20),20)),sg.T(' '*10),sg.B('Exit',size=(10,2),pad=((20,70),15))]    ]
 
     Window = sg.Window('Mass File Renamer by AU',layout,keep_on_top=True)
     
     while True:
         event, values = Window.read()
+        basepath = values['-IN-']
         if event in (sg.WINDOW_CLOSED, 'Exit'):
             break
+        elif event == 'Get Files List':
+            if values['-IN-']=='':
+                sg.popup('Please select a folder to perform operations in.',keep_on_top=True)
+            else:
+                sg.popup_scrolled('\n'.join(os.listdir(basepath)),title='List of Files')
         elif event=='rename-button':
             if values['-IN-']=='':
                 sg.popup('Please select a folder to perform operations in.',keep_on_top=True)
             else:
-                basepath = values['-IN-']
+                # basepath = values['-IN-']
                 for filename in os.listdir(basepath):
                     file_rename(Window, basepath, filename)
                 Window.close()
