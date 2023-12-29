e500d43fe6c28e3afda238f0483f159bb08f2c94
Anant Upadhyay <anant13u@gmail.com>
2023-12-29 18:27:27 +0530

    In progress of implementing GUI in the script.

--- My Projects/Photos Management Scripts/Convert HEIC to JPEG.py
+++ My Projects/Photos Management Scripts/Convert HEIC to JPEG.py
@@ -4,6 +4,40 @@ from pathlib import Path
 from pillow_heif import register_heif_opener
 from PIL import Image
 
+
+
+
+from pathlib import Path
+import PySimpleGUI as sg
+import os
+import traceback
+
+# sg.theme_previewer()
+sg.theme('DarkGreen7')
+layout = [  [sg.T('\nSelect folder:',pad=((30,15),20),s=(40,2)),sg.FolderBrowse(k='input-folder',pad=((0,20),20),s=(15,2))],
+            [sg.B('Organise Sizewise',pad=(70,0),s=(20,2)), sg.B('Exit',pad=((0,60),20),s=(20,2))]    ]
+
+window=sg.Window('Organise Files Sizewise', layout, keep_on_top=True,grab_anywhere=True)
+
+while True:
+    event, values = window.read()
+    if event in (sg.WINDOW_CLOSED, 'Exit'):
+        break
+    if values['input-folder']=='':
+        sg.popup('Please select a folder first.')
+    else:
+        basepath = values['input-folder']
+        print(basepath)
+        for file in os.listdir(basepath):
+
+
+
+
+
+
+
+
+
 # Register the HEIF opener with the PIL library
 register_heif_opener()
 
