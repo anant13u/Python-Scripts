a3cdf317575a61b863f5db5ce655d723e0959b7b
Anant Upadhyay <anant13u@gmail.com>
2024-01-19 15:15:03 +0530

    Commented out code that prints out each directory and its files separately as that's already covered in root operations. Also added code to adjust width of the first column based on the longest cell value.

--- My Projects/Get Files and Export to Excel.py
+++ My Projects/Get Files and Export to Excel.py
@@ -7,7 +7,7 @@ sg.theme('Reddit')
 
 filelist=[]
 
-layout = [  [sg.T('Select Folder',s=(30,3),pad=((40,20),10)), sg.FolderBrowse(key='-basepath-',s=(15,2),pad=(40,10))],
+layout = [  [sg.Text('Select Folder',s=(30,3),pad=((40,20),10),justification="center"), sg.FolderBrowse(key='-basepath-',s=(15,2),pad=(40,10))],
             # [sg.Radio('Full name?','1')],
 			[sg.B('Generate List',s=(15,2),pad=((100,30),10)), sg.B('Exit',s=(15,2),pad=(70,10))]  ]
 
@@ -23,30 +23,40 @@ while True:
     elif event == 'Generate List':
         # Window.close()
         for root, dirs, files in os.walk(basepath):
-            filelist.append(f'There are {len(dirs)} folders and {len(files)} files in {root}.\n')
+            filelist.append(f'There are {len(dirs)} folders and {len(files)} files in {root}\n'
+                            f'{root}')
             for entry in os.listdir(root):
                 filelist.append(entry)
-            for dir in dirs:
-                filelist.append(f'\nParent Directory: {Path(root,dir)}')
-                for entry in os.listdir(Path(root,dir)):
-                    filelist.append(entry)
+
+            # for dir in dirs:
+            #     filelist.append(f'\nParent Directory: {Path(root,dir)}')
+            #     for entry in os.listdir(Path(root,dir)):
+            #         filelist.append(entry)
             filelist.append('\n')
         filelist = '\n'.join(filelist)
         sg.popup_scrolled(filelist, title='Filelist')
 
-# Create a new Excel workbook and select the active sheet
-workbook = openpyxl.Workbook()
-sheet = workbook.active
+        # Create a new Excel workbook and select the active sheet
+        workbook = openpyxl.Workbook()
+        sheet = workbook.active
 
-# Write the list items to the Excel sheet column
-row_number = 1  # Specify the column number where you want to display the data
-for list_item in filelist.split('\n'):
-    sheet.cell(row=row_number, column=1, value=list_item)
-    row_number += 1
+        # Write the list items to the Excel sheet column
+        row_number = 1  # Specify the column number where you want to display the data
+        for list_item in filelist.split('\n'):
+            sheet.cell(row=row_number, column=1, value=list_item)
+            row_number += 1
+        
+        # Auto-adjust column width
+        max_length = 0
+        column = sheet['A']
+        for cell in column:
+            if len(str(cell.value))>max_length:
+                max_length=len(cell.value)
+        adjusted_width = max_length+1
+        sheet.column_dimensions['A'].width = adjusted_width
 
-# Save the workbook to a file
-workbook.save(Path(basepath,f'Files from {Path(basepath).parent.name}.xlsx'))
-# workbook.
+        # Save the workbook to a file
+        workbook.save(Path(basepath,f'Files from {Path(basepath).name}.xlsx'))
 
 
 
