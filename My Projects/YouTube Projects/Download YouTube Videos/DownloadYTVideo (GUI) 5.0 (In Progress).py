edaa5bcba8ee96acf517be3a626e43203a8e7c03
Anant Upadhyay <anant13u@gmail.com>
2024-01-26 22:52:42 +0530

    Implemented progress bar for video download process, utilizing PySimpleGUI's capabilities. Integrated the progress bar into the download window layout, updating it dynamically during download. Modified the 'download_video' function to include a progress callback for updating the bar. Resolved variable scope issues and improved error handling for a seamless user experience.

--- My Projects/YouTube Projects/Download YouTube Videos/DownloadYTVideo (GUI) 5.0 (In Progress).py
+++ My Projects/YouTube Projects/Download YouTube Videos/DownloadYTVideo (GUI) 5.0 (In Progress).py
@@ -4,10 +4,13 @@ from pathlib import Path
 import time
 import PySimpleGUI as sg
 from pytube import YouTube
+from pytube.cli import on_progress
 from datetime import datetime
 
 sg.theme('darkamber') # sg.theme_previewer()
 sg.set_options(font=('Calibri',11)) # https://stackoverflow.com/a/67155752/18791688
+sg.ProgressBar(100, size=(20, 30), key='-progress')
+
 
 folderBrowse = sg.FolderBrowse('Select Download Folder',key='download_folder', size=(45,2), pad=(40,(30,10)))
 proceedButton = sg.B('Proceed', size=(18,2), pad=(45,30))
@@ -40,28 +43,37 @@ def download_another_video():
     while True:
         event, values = sg.Window('Download another video?',
                         [   [sg.T('Do you wish to download another video?', pad=(50,15))],
-                            [sg.B('Yes', size=(15,1), pad=(30,15)),sg.B('No', size=(15,1), pad=(30,15))]    ]).read(close=True) # close=True closes the Window after getting the input in form of Yes or No
+                            [sg.B('Yes', size=(15,1), pad=(30,15)),sg.B('No', size=(15,1), pad=(30,15))]    ], keep_on_top=True).read(close=True) # close=True closes the Window after getting the input in form of Yes or No
         if event=='Yes':
             mains()
-        else:
-            time.sleep(1)
+        # else:
+        #     time.sleep(1)
         break
 
 
-def download_video(video_name, channel_name, url, yt, stream):
+def download_video(video_name, channel_name, url, yt, stream, download_video_window):
+
+    def on_progress(stream, chunk, bytes_remaining):
+        size = stream.filesize
+        bytes_downloaded = size - bytes_remaining
+        progress_percent = (bytes_downloaded / size) * 100
+        download_video_window['-progress-'].update_bar(progress_percent)
+
+    yt.register_on_progress_callback(on_progress)
+    
     # Below we are replacing the characters in the replacers_dict dictionary present in video name with an empty string.
     for key, value in replacers_dict.items():
         video_name = video_name.replace(key,value)
     print(f'\n Video name: "{video_name}"')
-    final_file = Path.joinpath(download_path,f'{channel_name} - {video_name}.mp4') # Changing filename to now have the entire path, the channel name, and the mp4 extension.
+    final_file = Path(download_path,f'{channel_name} - {video_name}.mp4') # Changing filename to now have the entire path, the channel name, and the mp4 extension.
     print(f'\n {final_file}')
     # Below we are checking if the file already exists in the download path. If it does, it will open
     # the download path in the browser and show the file already exists.
     if Path(final_file).exists():
-        sg.popup(f'The file already exists at {download_path}')
+        sg.popup(f'The file already exists at {download_path}', keep_on_top=True)
     else:
         # Below we will show a popup with the text "Downloading..." and the duration of the popup is (size of the video)/3.
-        sg.popup_auto_close('Downloading...',auto_close_duration=5)
+        # sg.popup_auto_close('Downloading...',auto_close_duration=5)
         # auto_close_duration=stream.filesize_approx/(1024*1024*3)
         try:
             print(download_path)
@@ -81,20 +93,20 @@ def download_video(video_name, channel_name, url, yt, stream):
                             f'Length: {yt.length//60} minutes, {yt.length%60} seconds.\n'
                             f'Size: {round(final_file.stat().st_size/(1024*1024), 2)} MB.\n'
                             f'Description: {yt.description}\n\n')
-        sg.popup(f'Your video: "{video_name}" has been downloaded and saved at - "{download_path}"')
+        sg.popup(f'Your video: "{video_name}" has been downloaded and saved at - "{download_path}"', keep_on_top=True)
     webbrowser.open(download_path) # This will open the directory where we've downloaded the video. Webbrowser module will be imported automatically.
     # return final_file # Using return to capture downloaded file's name so we can refer to only_download function later on and retrieve it.
 
 
 def mains():
-    download_video_layout = [   [sg.T('Please enter the URL for the YouTube video below:', pad = (20,20))],
+    fetch_details_layout = [    [sg.T('Please enter the URL for the YouTube video below:', pad = (20,20))],
                                 [sg.InputText(pyperclip.paste(),key='-URL-', pad = (20,10))],
                                 [sg.Ok('Fetch Video Details',size=(17,2), pad=(20,20)),sg.B('Exit',size=(17,2), pad=(20,20))]    ]
         
-    download_video_window = sg.Window('YouTube Video Downloader by AU', download_video_layout,keep_on_top=True)
+    fetch_details_window = sg.Window('YouTube Video Downloader by AU', fetch_details_layout,keep_on_top=True)
 
     while True:
-        event, values = download_video_window.read()
+        event, values = fetch_details_window.read()
         if event in (sg.WIN_CLOSED or 'Exit'):
             break
         elif values['-URL-']=='':
@@ -104,30 +116,33 @@ def mains():
         elif event=='Fetch Video Details':
             url = values['-URL-']
             try:
-                yt = YouTube(url)
+                yt = YouTube(url, on_progress_callback=on_progress)
+                yt.streams.filter(file_extension='mp4')
             except Exception as e:
-                download_video_window.close()
-                sg.popup(f'"yt = YouTube(url)" line could not be resolved.\nError Encountered: {e}', keep_on_top=True)
-                print(f'"yt = YouTube(url)" line could not be resolved.\nError Encountered: {e}')
+                fetch_details_window.close()
+                sg.popup(f'Error encountered: {e}', keep_on_top=True)
+                print(f'Error encountered: {e}')
                 download_another_video()
                 break
-            yt.streams.filter(file_extension='mp4')
             stream = yt.streams.get_by_itag(22)
             video_name = stream.title
             channel_name = yt.author
-            download_video_window.close()
+            fetch_details_window.close()
             print(video_name)
-            box_height = (len(video_name)//50) + 1
-            event, values = sg.Window('Want to Download This Video?',
-                [   [sg.T(f'Your video is "{video_name}"', s=(50,box_height), pad=(20,10))],
-                    [sg.T(f'Channel Name: {channel_name}', pad=(20,10))],
-                    [sg.T(f'Length of the video is {yt.length//60} minutes and {yt.length%60} seconds.', pad=(20,10))],
-                    [sg.T(f'Approx Size of the video is {round(stream.filesize_approx/(1024*1024),2)} MB.', pad=(20,10))],
-                    [sg.T(f'{yt.description}', size=(55,8), pad=(20,(10,25)))],
-                    [sg.B('Download', size=(22,2), pad=(30,20)), sg.B("Exit",size=(22,2), pad=(20,20))]    ], keep_on_top=True).read(close=True) # close=True closes the Window after getting the input in form of Yes or No
+            download_video_layout = [   [sg.T(f'Your video is "{video_name}"', s=(50,1), pad=(20,10))],
+                        [sg.T(f'Channel Name: {channel_name}', pad=(20,10))],
+                        [sg.T(f'Length of the video is {yt.length//60} minutes and {yt.length%60} seconds.', pad=(20,10))],
+                        [sg.T(f'Approx Size of the video is {round(stream.filesize_approx/(1024*1024),2)} MB.', pad=(20,10))],
+                        [sg.T(f'{yt.description}', size=(55,8), pad=(20,(10,25)))],
+                        [sg.ProgressBar(100, key='-progress-', s=(30,20), pad=(50,10))],
+                        [sg.B('Download', size=(22,2), pad=(30,20)), sg.B("Exit",size=(22,2), pad=(20,20))]    ] # close=True closes the Window after getting the input in form of Yes or No
+            download_video_window = sg.Window('YouTube Video Downloader by AU', download_video_layout, keep_on_top=True)
+            event, values = download_video_window.read()
 
             if event=='Download':
-                download_video(video_name, channel_name, url, yt, stream)
+                download_video(video_name, channel_name, url, yt, stream, download_video_window)
+
+            download_video_window.close()
 
             download_another_video()
 
