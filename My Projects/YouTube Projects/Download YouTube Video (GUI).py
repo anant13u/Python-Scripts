26afbbb0fddd6247056c1a9fd05db734a9b6e4ba
Anant Upadhyay <anant13u@gmail.com>
2024-01-26 23:23:08 +0530

    Moved out the latest download script to the root folder, and moved the older script to the older scripts folder.

--- My Projects/YouTube Projects/Download YouTube Video (GUI).py
+++ My Projects/YouTube Projects/Download YouTube Video (GUI).py
@@ -31,7 +31,6 @@ while True:
             getDWPathWindow.close()
             break
 
-# mew = time.sleep(1.5)
 replacers_dict = {'|':'', '।':'', ':':'', '/':'', '\\':'', '"':'', '*':'', '?':'', '<':'', '>':''}
 # replacers = ['|','|', ':', '/', '\\', '"', '*', '?', '<', '>']
 
@@ -59,43 +58,41 @@ def download_video(video_name, channel_name, url, yt, stream, download_video_win
         progress_percent = (bytes_downloaded / size) * 100
         download_video_window['-progress-'].update_bar(progress_percent)
 
+    # This line registers the on_progress function as a callback function for the YouTube object yt.
+    # When a video download is in progress, the on_progress function will be automatically 
+    # called periodically to update the progress of the download.
     yt.register_on_progress_callback(on_progress)
-    
+
     # Below we are replacing the characters in the replacers_dict dictionary present in video name with an empty string.
     for key, value in replacers_dict.items():
         video_name = video_name.replace(key,value)
     print(f'\n Video name: "{video_name}"')
     final_file = Path(download_path,f'{channel_name} - {video_name}.mp4') # Changing filename to now have the entire path, the channel name, and the mp4 extension.
     print(f'\n {final_file}')
-    # Below we are checking if the file already exists in the download path. If it does, it will open
-    # the download path in the browser and show the file already exists.
+    # Below we are checking if the file already exists in the download path.
     if Path(final_file).exists():
         sg.popup(f'The file already exists at {download_path}', keep_on_top=True)
     else:
-        # Below we will show a popup with the text "Downloading..." and the duration of the popup is (size of the video)/3.
-        # sg.popup_auto_close('Downloading...',auto_close_duration=5)
-        # auto_close_duration=stream.filesize_approx/(1024*1024*3)
         try:
             print(download_path)
             print(Path(final_file).name)
             stream.download(output_path=download_path, filename=Path(final_file).name) # Download path is already provided at the beginning of the script.
+            print(f'\n Downloaded filename: \n "{final_file}"')
+            curr_datetime = datetime.now().strftime('%d/%m/%y %H:%M:%S')
+            with open(Path.joinpath(download_path,'Downloaded Videos.txt'),'a+',encoding='utf-8') as curr_log:
+                curr_log.write(f'{curr_datetime}\n'
+                                f'Download Location: {download_path}\n'
+                                f'Video: "{video_name}".\n'
+                                f'URL: {url}\n'
+                                f'Channel: {channel_name}.\n'
+                                f'Length: {yt.length//60} minutes, {yt.length%60} seconds.\n'
+                                f'Size: {round(final_file.stat().st_size/(1024*1024), 2)} MB.\n'
+                                f'Description: {yt.description}\n\n')
+            sg.popup(f'Your video: "{video_name}" has been downloaded and saved at - "{download_path}"', keep_on_top=True)
         except Exception as e:
             sg.popup(f'Error encountered: {e}')
             print(f'Error encountered: {e}')
-        print(f'\n Downloaded filename: \n "{final_file}"')
-        curr_datetime = datetime.now().strftime('%d/%m/%y %H:%M:%S')
-        with open(Path.joinpath(download_path,'Downloaded Videos.txt'),'a+',encoding='utf-8') as curr_log:
-            curr_log.write(f'{curr_datetime}\n'
-                            f'Download Location: {download_path}\n'
-                            f'Video: "{video_name}".\n'
-                            f'URL: {url}\n'
-                            f'Channel: {channel_name}.\n'
-                            f'Length: {yt.length//60} minutes, {yt.length%60} seconds.\n'
-                            f'Size: {round(final_file.stat().st_size/(1024*1024), 2)} MB.\n'
-                            f'Description: {yt.description}\n\n')
-        sg.popup(f'Your video: "{video_name}" has been downloaded and saved at - "{download_path}"', keep_on_top=True)
     webbrowser.open(download_path) # This will open the directory where we've downloaded the video. Webbrowser module will be imported automatically.
-    # return final_file # Using return to capture downloaded file's name so we can refer to only_download function later on and retrieve it.
 
 
 def mains():
@@ -116,7 +113,7 @@ def mains():
         elif event=='Fetch Video Details':
             url = values['-URL-']
             try:
-                yt = YouTube(url, on_progress_callback=on_progress)
+                yt = YouTube(url) # on_progress_callback=on_progress
                 yt.streams.filter(file_extension='mp4')
             except Exception as e:
                 fetch_details_window.close()
