14ec48e0dd9c08732835f012aaec68bd5e515540
Anant Upadhyay <anant13u@gmail.com>
2024-01-30 17:31:01 +0530

    Refactored code for appending data to the sheet and auto-adjusting column width. Used sheet.append for simplicity. Also modifed filelist to organize file information as lists within it.

--- My Projects/Get Files and Export to Excel.py
+++ My Projects/Get Files and Export to Excel.py
@@ -13,28 +13,33 @@ def create_excel(basepath, filelist, Window):
     sheet = workbook.active
 
     # Write the list items to the Excel sheet column
-    row_number = 1  # Specify the row number where you want to display the data
+    # row_number = 1  # Specify the row number where you want to display the data
     for list_item in filelist:
-        sheet.cell(row=row_number, column=1, value=list_item)
-        row_number += 1
+        # sheet.cell(row=row_number, column=1, value=list_item)
+        # row_number += 1
+        sheet.append(list_item)
     filelist=[]
+
     
     # Auto-adjust column width
-    max_length = 0
-    column = sheet['A']
-    for cell in column:
-        if len(str(cell.value))>max_length:
-            max_length=len(cell.value)
-    adjusted_width = max_length+1
-    sheet.column_dimensions['A'].width = adjusted_width
+    for column in sheet.columns:
+        max_length = 0
+    # column = sheet['A']
+        for cell in column:
+            if len(str(cell.value))>max_length:
+                max_length=len(str(cell.value))
+        max_length = max_length + 2
+    # sheet.column_dimensions['A'].width = adjusted_width
+        sheet.column_dimensions[column[0].column_letter].width = max_length
+
 
     try:
-        curr_date_time = datetime.now().strftime('%d-%m-%Y %H-%M-%S')
+        curr_date_time = datetime.now().strftime('%d-%m-%Y %H_%M_%S')
         # Save the workbook to a file
         workbook.save(Path(basepath,f'Files from {Path(basepath).name} ({curr_date_time}).xlsx'))
     except PermissionError:
         sg.popup('The output Excel file is already open. Please close and try again.')
-    Window.reappear()
+    # Window.reappear()
 
     # mains()
 
@@ -61,22 +66,25 @@ def mains():
         elif values['-basepath-']=='':
             sg.popup('Please select a folder to perform operations in.')
         elif event == 'Generate List':
-            filelist={}
-            filelist.append('Full Path|File Name|File Size')
+            filelist=[]
             print(basepath)
-            for root, dirs, files in os.walk(basepath):
-                # filelist.append(f'There are {len(dirs)} folders and {len(files)} files in {root}\n')
-                for entry in os.listdir(root):
-                    if values['-includesize-']==True:
+            if values['-includesize-']:
+                filelist.append(['File Name','File Size (in MB)','File Type','Full Path'])
+                for root, dirs, files in os.walk(basepath):
+                    # filelist.append(f'There are {len(dirs)} folders and {len(files)} files in {root}\n')
+                    for entry in os.listdir(root):
                         file_size = os.path.getsize(Path(root, entry))/(1024*1024) # file_size is 5.0469865798950195
-                        filelist.append(f'{Path(root, entry)}|{entry}|{round(file_size,2)}')
-                    else:
-                        filelist.append(entry)
-                filelist.append('\n')
+                        filelist.append([entry, round(file_size, 2), Path(root, entry).suffix, str(Path(root, entry))]) # .replace(".","")
+            else:
+                filelist.append(['File Name','File Type','Full Path'])
+                for root, dirs, files in os.walk(basepath):
+                    for entry in os.listdir(root):
+                        filelist.append([entry, Path(root, entry).suffix, str(Path(root, entry))])
+            # filelist.append('\n')
             # filelist = '\n'.join(filelist)
             # sg.popup_scrolled('\n'.join(filelist), title='Filelist')
 
-            Window.disappear()
+            # Window.disappear()
 
             create_excel(basepath, filelist, Window)
 
