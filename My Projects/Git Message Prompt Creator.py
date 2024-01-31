29c1e8e8bf3a490bcde71e2b8e6507d7413721b9
Anant Upadhyay <anant13u@gmail.com>
2024-01-31 18:31:27 +0530

    Added ability for the window to be grabbed anywhere and moved. Commented out code that opened instance of ChatGPT in the browser.

--- My Projects/Git Message Prompt Creator.py
+++ My Projects/Git Message Prompt Creator.py
@@ -10,7 +10,7 @@ layout = [  [sg.T(' '*30), sg.T('Initial Code'),sg.T(' '*73), sg.T('Current Code
             [sg.T(' '*20), sg.B('Create and copy prompt',s=(25,2)),sg.T(' '*59), sg.B('Exit',s=(9,2))],
             [sg.T()]  ]
 
-Window = sg.Window('Git Commit Message Creator - AU',layout, keep_on_top=True)
+Window = sg.Window('Git Commit Message Creator - AU',layout, keep_on_top=True, grab_anywhere=True)
 
 while True:
     event, values = Window.read()
@@ -22,7 +22,7 @@ while True:
         # sg.popup(initialCode,currentCode)
         prompt = f'I need you to write a Git commit message (preferably in 3-4 lines max) for the code changes made below:\nPrevious code:\n{initialCode}\n\nCurrent Code:\n{currentCode}' 
         pyperclip.copy(prompt)
-        webbrowser.open('https://chat.openai.com/')
+        # webbrowser.open('https://chat.openai.com/')
 
 
 # On Linux, install xclip, xsel, or wl-clipboard (for "wayland" sessions) via package manager. For example, in Debian:
