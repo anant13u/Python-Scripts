7ac195eab692783c51f7d31ca0d4dc3524dec02d
Anant Upadhyay <anant13u@gmail.com>
2024-03-07 21:13:36 +0530

    Named the button for the appropriate action.

--- My Projects/Clean Temp Files from a Folder.py
+++ My Projects/Clean Temp Files from a Folder.py
@@ -9,7 +9,7 @@ selectFolderText = sg.Text('Select Folder',s=(30,2),pad=((40,20),10))
 folderBrowse = sg.FolderBrowse(key='-basepath-',s=(15,2),pad=(40,10))
 
 layout = [  [selectFolderText, folderBrowse],
-            [sg.B('Generate List',s=(15,2),pad=(30,10)), sg.B('Exit',s=(15,2),pad=(70,10))]  ]
+            [sg.B('Clean Temp Files',s=(15,2),pad=(30,10)), sg.B('Exit',s=(15,2),pad=(70,10))]  ]
 
 Window = sg.Window('Generate list of files', layout)
 
@@ -20,7 +20,9 @@ while True:
         break
     elif values['-basepath-']=='':
         sg.popup('Please select a folder to perform operations in.')
-    elif event == 'Generate List':
+    elif event == 'Clean Temp Files':
+        for root, dirs, files in os.walk(directory):
+            for file in files:
         subprocess.Popen(['explorer.exe', basepath])
         print(basepath)
         
