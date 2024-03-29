9b665c7ce97334448ced2782aaa18958c672b63d
Anant Upadhyay <anant13u@gmail.com>
2024-03-29 23:57:30 +0530

    Increased padding for the 1st button in 2nd row.

--- My Projects/Extract RAR Files in their own folders.py
+++ My Projects/Extract RAR Files in their own folders.py
@@ -7,7 +7,7 @@ import traceback
 # sg.theme_previewer()
 sg.theme('DarkGreen7')
 layout = [  [sg.T('\nSelect folder:',pad=((30,15),20),s=(40,2)),sg.FolderBrowse(k='input-folder',pad=((0,20),20),s=(15,2))],
-            [sg.B('Extract',pad=(70,0),s=(15,2)), sg.B('Exit',pad=((0,60),20),s=(15,2))]    ]
+            [sg.B('Extract',pad=(90,0),s=(15,2)), sg.B('Exit',pad=((0,60),20),s=(15,2))]    ]
 
 window=sg.Window('Extract RAR Files', layout, keep_on_top=True,grab_anywhere=True)
 
