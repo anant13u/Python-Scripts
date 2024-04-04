ef9e8651b1542c75bb2b6f1c5dcd4e40dfca3df8
Anant Upadhyay <anant13u@gmail.com>
2024-04-04 21:32:34 +0530

    Made an initial layout for the GUI.

--- My Projects/Video Audio Manipulation/Trim Videos.py
+++ My Projects/Video Audio Manipulation/Trim Videos.py
@@ -3,3 +3,9 @@ import PySimpleGUI as sg
 import subprocess
 from pathlib import Path
 
+sg.theme('Reddit')
+# sg.theme_previewer()
+
+
+layout = [  [sg.T('Select Video'), sg.FileBrowse()],
+            [sg.B('Generate List',s=(15,2),pad=(30,10)), sg.B('Exit',s=(15,2),pad=(70,10))]  ]
