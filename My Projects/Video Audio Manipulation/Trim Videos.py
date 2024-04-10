7988f1f141fd0ef615a5f9c5182afb1398fc8cdf
Anant Upadhyay <32584210+anant13u@users.noreply.github.com>
2024-04-10 17:49:11 +0530

    Assigned keys to the input-boxes to be able to assign their values to a variable.

--- My Projects/Video Audio Manipulation/Trim Videos.py
+++ My Projects/Video Audio Manipulation/Trim Videos.py
@@ -8,8 +8,8 @@ sg.theme('Reddit')
 
 
 layout = [  [sg.T('Select Video'), sg.FileBrowse(key='video_file')],
-            [sg.T('Start Time'), sg.I('',s=(15,2))],
-            [sg.T('End Time'), sg.I('',s=(15,2))]  ]
+            [sg.T('Start Time'), sg.I('', key = 'start_time', s=(15,2))],
+            [sg.T('End Time'), sg.I('', key = 'end_time', s=(15,2))]  ]
             
 Window = sg.Window('Trim Videos', layout)
 
