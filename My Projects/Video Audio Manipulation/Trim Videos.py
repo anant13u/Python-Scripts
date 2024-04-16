6761af5f01cb67b9122c47545e05ebae02551c34
Anant Upadhyay <anant13u@gmail.com>
2024-04-16 19:37:02 +0530

    Implemented a function to convert time string to seconds for precise trimming. Added code to trim video based on start and end time input.

--- My Projects/Video Audio Manipulation/Trim Videos.py
+++ My Projects/Video Audio Manipulation/Trim Videos.py
@@ -2,6 +2,7 @@ import moviepy.editor as mp
 import PySimpleGUI as sg
 import subprocess
 from pathlib import Path
+import os
 
 sg.theme('Reddit')
 # sg.theme_previewer()
@@ -14,17 +15,30 @@ layout = [  [sg.T('Select Video', s=(35,2), pad=(30,20)), sg.FileBrowse(key='vid
             
 Window = sg.Window('Trim Videos', layout)
 
+def time_to_seconds(time_str):
+    # Split the time string into hours, minutes, and seconds
+    hours, minutes, seconds = map(int, time_str.split(':'))
+    
+    # Convert the time to seconds
+    total_seconds = hours * 3600 + minutes * 60 + seconds
+    return total_seconds
+
 while True:
     event, values = Window.read()
-    videoFile = Path(values['video_file'])
+    videoFile = values['video_file']
+    # print(videoFile)
+    # print(os.path.basename(videoFile).split('.')[0])
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
     elif values['video_file']=='':
         sg.popup('Please select a video to perform operations on.')
     elif values['start_time'] == '' or values['end_time']=='':
         sg.popup('Please enter start and end time for trimming the video.')
-
     elif event == 'Trim Video':
+        filename = os.path.basename(videoFile).split('.')[0]
+        startTime = time_to_seconds(values['start_time'])
+        endTime = time_to_seconds(values['end_time'])
         ourClip = mp.VideoFileClip(videoFile)
-        trimmedClip = ourClip.subclip
+        trimmedClip = ourClip.subclip(startTime, endTime)
+        trimmedClip.write_videofile(videoFile.replace(filename, f'{filename}_{startTime}_{endTime}'), codec='libx264')
 
