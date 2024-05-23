140f27ffdd3e8cb669b68ae0704669f4360189d3
Anant <anant13u@gmail.com>
2024-05-23 13:27:46 +0530

    Added a popup that'll inform the users once the files have been moved to the root folder.

---
+++ My Projects/File Renamers/Move Files to Root Folder.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+import PySimpleGUI as sg
+import traceback
+import shutil
+
+# sg.theme_previewer()
+sg.theme('DarkGreen7')
+
+move_button = sg.B('Move Files to Root Folder', k='-move_files-', pad=(40,0), s=(20,3))
+layout = [  [sg.T('Select folder:', pad=(40,20), s=(40,2)),sg.FolderBrowse(k='input-folder', pad=((0,20),30), s=(15,2))],
+            [move_button, sg.B('Exit', pad=(40,30), s=(15,3))]  ]
+
+window = sg.Window('Move Files to Root Folder', layout, keep_on_top=True, grab_anywhere=True)
+
+while True:
+    event, values = window.read()
+    basepath = values['input-folder']
+    print(basepath)
+    if event in (sg.WINDOW_CLOSED, 'Exit'):
+        break
+    if values['input-folder']=='':
+        sg.popup('Please select a folder first.', keep_on_top=True)
+    elif event=='-move_files-':
+        # Traverse through the directory tree using os.walk
+        for root, directories, files in os.walk(basepath):
+            for curr_dir in directories:
+                # Iterate through files in each directory
+                for file in os.listdir(Path(root,curr_dir)):
+                    file_curr_path = Path(root,curr_dir,file)
+                    file_dest_path = Path(root,file)
+                    try:
+                        shutil.move(file_curr_path, file_dest_path)
+                    except Exception as e:
+                        error_info = traceback.format_exc()
+                        sg.popup(f'Got an error while renaming {file}:\n{error_info}', keep_on_top=True)
+                        print(f'Got an error while renaming {file}:\n{error_info}')
+                sg.popup('All files have been moved to the root folder.')
+                        
