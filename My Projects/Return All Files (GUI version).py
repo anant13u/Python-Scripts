222797a9b32eb8b705ab1201dc0fe68b3ed1349d
Anant <anant13u@gmail.com>
2024-05-28 20:02:33 +0530

    Removed redundant code.

--- My Projects/Return All Files (GUI version).py
+++ My Projects/Return All Files (GUI version).py
@@ -29,8 +29,6 @@ while True:
 				current_file = os.path.join(root,file)
 				if FileNotFoundError:
 					pass
-				# current_file_size = round(os.path.getsize(current_file)/(1024*1024),2)
-				# total_files_size += current_file_size # Adding the current file's size to the variable total_files_size to print at the end.
 				filelist.append(file) # \n (Size of file is {current_file_size} MB)')
 					# filelist.append(f'\n Folder: {root}\n (Size of folder is {current_file_size} MB)')
 		filelist = '\n'.join(filelist)
