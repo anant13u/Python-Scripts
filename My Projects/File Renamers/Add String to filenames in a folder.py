9ad82a500469d1ae9fdc4e8c6048c236d405efff
Anant Upadhyay <anant13u@gmail.com>
2024-06-17 23:23:12 +0530

    Set a size for the inputbox.

--- My Projects/File Renamers/Add String to filenames in a folder.py
+++ My Projects/File Renamers/Add String to filenames in a folder.py
@@ -11,7 +11,7 @@ selectFolderText = sg.Text('Select Folder',s=(30,2),pad=((40,20),10))
 folderBrowse = sg.FolderBrowse(key='-basepath-',s=(15,2),pad=(40,10))
 
 layout = [  [selectFolderText, folderBrowse],
-            [sg.T('String', pad=((40,60),30)), sg.I()],
+            [sg.T('String', pad=((40,60),30)), sg.I(s=40)],
             [sg.B('Generate List',s=(15,2),pad=(30,10)), sg.B('Exit',s=(15,2),pad=(70,10))]  ]
 
 Window = sg.Window('Generate list of files', layout)
