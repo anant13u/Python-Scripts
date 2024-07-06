869b55e7158ea7818802b559749b7461eb396699
Anant Upadhyay <anant13u@gmail.com>
2024-07-06 19:06:12 +0530

    added code for renaming the files by adding the string.

--- My Projects/File Renamers/Add String to filenames in a folder.py
+++ My Projects/File Renamers/Add String to filenames in a folder.py
@@ -7,12 +7,14 @@ import subprocess
 sg.theme('Reddit')
 # sg.theme_previewer()
 
-selectFolderText = sg.Text('Select Folder',s=(30,2),pad=((40,20),20))
+selectFolderText = sg.Text('Select Folder',s=(30,2),pad=((40,20),30))
 folderBrowse = sg.FolderBrowse(key='-basepath-',s=(15,2),pad=(40,10))
 
 layout = [  [selectFolderText, folderBrowse],
-            [sg.T('String', pad=((40,100),30)), sg.I(k='joiner-string', s=40)],
-            [sg.B('Add string at the beginning',s=(15,2),pad=(55,30)), sg.B('Add string at the end',s=(15,2),pad=(55,30)), sg.B('Exit',s=(15,2),pad=(70,10))]  ]
+            [sg.T('String', pad=((40,100),20)), sg.I(k='joiner-string', s=40, enable_events=True)],
+            [sg.T('filename.mp4',k='begin-textbox', s=(30,2), p=((80,0),20)), sg.T('filename.mp4', k='end-textbox', s=(20,2))],
+            [sg.B('Add string at the beginning',s=(22,2),pad=((50,30),20)), sg.B('Add string at the end',s=(22,2),pad=(20,20))],
+            [sg.B('Exit',s=(15,2),pad=(190,30))]  ]
 
 Window = sg.Window('Generate list of files', layout)
 
@@ -20,27 +22,25 @@ while True:
     event, values = Window.read()
     basepath = Path(values['-basepath-'])
     joinerString = values['joiner-string']
+    Window['begin-textbox'].update(f'{joinerString}filename.mp4')
+    Window['end-textbox'].update(f'filename{joinerString}.mp4')
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
     elif values['-basepath-']=='':
         sg.popup('Please select a folder to perform operations in.')
     elif event == 'Add string at the beginning':
-        # basepath = Path(values['-basepath-'])
-        # joinerString = values['joiner-string']
+        print(basepath)
+        for entry in os.listdir(basepath):
+            os.rename(Path(basepath, entry), Path(basepath, f'{joinerString}{entry}'))
         # subprocess.Popen(['explorer.exe', basepath]) # Below 2 lines give the same result.
         # subprocess.run(['explorer', basepath])
         # subprocess.Popen(['explorer', basepath])
-        print(basepath)
-        for entry in os.listdir(basepath):
-            curr_name = Path(basepath, entry)
-            print(f'{joinerString}{entry}')
-            print(os.path.splitext(entry)[0])
     elif event == 'Add string at the end':
         for entry in os.listdir(basepath):
             justFilename = os.path.splitext(entry)[0]
             fileExt = os.path.splitext(entry)[1]
-            # currName = Path(basepath, entry)
-            print(f'{justFilename}{joinerString}{fileExt}')
+            os.rename(Path(basepath, entry), Path(basepath, f'{justFilename}{joinerString}{fileExt}'))
+        # subprocess.Popen(['explorer.exe', basepath]) # Below 2 lines give the same result.
 
 
 
