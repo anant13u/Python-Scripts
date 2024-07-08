bdc16db8aa0b1318810eee243efbbe29b9d7740f
Anant <anant13u@gmail.com>
2024-07-08 11:30:53 +0530

    Changed vertical padding for the last row buttons.

--- My Projects/File Renamers/MassFileRenamer (GUI) (Renames in one go).py
+++ My Projects/File Renamers/MassFileRenamer (GUI) (Renames in one go).py
@@ -7,7 +7,7 @@ sz= size=(20,2)
 layout = [  [sg.Text('Please select the folder where you want to rename the files',size=(30,2), pad=(20,15)), sg.FolderBrowse(key='-IN-',size=(15,2),pad=((70,20),15))],
             [sg.Text('Please enter the string you want to rename',sz,pad=(20,15)),sg.Input('',key='old-string',pad=(10,10),size=(40,2))],
             [sg.Text('Please enter the new text',sz,pad=(20,15)),sg.Input('',key='new-string',size=(40,2),pad=(10,10))],
-            [sg.Button('Rename',size=(13,2),pad=((90,20),15)),sg.T(' '*10),sg.Button('Exit',size=(13,2),pad=((20,70),15))]    ]
+            [sg.Button('Rename',size=(13,2),pad=((90,20),30)),sg.T(' '*10),sg.Button('Exit',size=(13,2),pad=((20,70),15))]    ]
 
 Window = sg.Window('Mass File Renamer by AU',layout,keep_on_top=True, grab_anywhere=True)
 
