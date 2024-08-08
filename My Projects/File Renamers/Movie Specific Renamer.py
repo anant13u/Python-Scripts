d28079c42ab42a033ba70735edcbcbc46bbe1398
Anant Upadhyay <anant13u@gmail.com>
2024-08-08 13:24:44 +0530

    Removed code that creates folders for all files in the selected directory.

--- My Projects/File Renamers/Movie Specific Renamer.py
+++ My Projects/File Renamers/Movie Specific Renamer.py
@@ -6,12 +6,11 @@ import traceback
 # sg.theme_previewer()
 sg.theme('DarkGreen7')
 
-rename_button = sg.B('Rename Movies as\nper Folder Name', k='-rename_movies-', pad=(40,0), s=(20,3))
-create_folders_button = sg.B('Get the Files\ntheir own Folders', k='-create_folders-', s=(20,3))
-layout = [  [sg.T('\nSelect folder:', pad=(70,20), s=(40,2)),sg.FolderBrowse(k='input-folder', pad=((0,20),30), s=(15,2))],
-            [rename_button, create_folders_button, sg.B('Exit', pad=(40,30), s=(15,3))]  ]
+rename_button = sg.B('Rename Movies as\nper Folder Name', k='-rename_movies-', pad=((110,40),0), s=(20,3))
+layout = [  [sg.T('Select folder:', pad=((50,70),20), s=(40,2)),sg.FolderBrowse(k='input-folder', pad=((0,20),30), s=(15,2))],
+            [rename_button, sg.B('Exit', pad=(40,30), s=(15,3))]  ]
 
-window=sg.Window('Rename Movies and Subtitles as per parent folder', layout, keep_on_top=True,grab_anywhere=True)
+window=sg.Window('Rename Movies and Subtitles as per parent folder', layout, keep_on_top=True, grab_anywhere=True)
 
 while True:
     event, values = window.read()
@@ -26,9 +25,9 @@ while True:
         for root, directories, files in os.walk(basepath):
             for curr_dir in directories:
                 # Iterate through files in each directory
-                for file in os.listdir(Path(root,curr_dir)):
+                for file in os.listdir(Path(root, curr_dir)):
                     file_ext = Path(file).suffix.lower()
-                    file_size = os.path.getsize(Path(root,curr_dir,file))/(1024*1024) # file_size is 5.0469865798950195
+                    file_size = os.path.getsize(Path(root,curr_dir, file))/(1024*1024) # file_size is 5.0469865798950195
                     # Check conditions for renaming:
                     # - Ensure the file's base name is not the same as its parent directory (avoid unnecessary renaming).
                     # - File size should be greater than 50 MB if extension is '.mp4', '.mkv' or '.avi'.
@@ -36,25 +35,12 @@ while True:
                     # splitext is used to split the extension from a pathname.
                     if os.path.splitext(file)[0] != curr_dir and ((file_size>50 and file_ext in ('.mp4','.mkv','.avi')) or file_ext=='.srt'):
                         try:
-                            new_filename = file.replace(os.path.splitext(file)[0],curr_dir)
-                            os.rename(Path(root,curr_dir,file),Path(root,curr_dir,new_filename))
+                            new_filename = file.replace(os.path.splitext(file)[0], curr_dir)
+                            os.rename(Path(root,curr_dir,file),Path(root,curr_dir, new_filename))
                         except Exception as e:
                             error_info = traceback.format_exc()
                             sg.popup(f'Got an error while renaming {file}:\n{error_info}', keep_on_top=True)
                             print(f'Got an error while renaming {file}:\n{error_info}')
-
-    elif event=='-create_folders-':
-        for item in os.listdir(basepath):
-            file_ext = Path(item).suffix
-            item_folder_path = Path(basepath, item.replace(file_ext, ''))
-            if Path(basepath, item).is_file() and not os.path.exists(item_folder_path):
-                try:
-                    os.mkdir(item_folder_path)
-                    os.rename(Path(basepath, item), Path(item_folder_path, item))
-                except Exception as e:
-                    sg.popup(f'Got an error:\n{e}', keep_on_top=True)
-                    print(f'Got an error:\n{e}')
-        sg.popup('Movie Renaming completed.',keep_on_top=True)
 
 
 # Adding functionality to create folder for files without folder and move them inside.
