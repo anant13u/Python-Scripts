62edba19c9f427642565e60314f703218fd930e6
Anant Upadhyay <32584210+anant13u@users.noreply.github.com>
2024-10-19 23:50:32 +0530

    Improved f-string.

--- For Reference/Notes & Points/x3.py
+++ For Reference/Notes & Points/x3.py
@@ -9,6 +9,6 @@ sqt(7)
 def sqt1():
     x = int(input())
     y = x**(1/2)
-    print(f'The Square Root of {x} is ' + str(round(y,3)))
+    print(f'The Square Root of {x} is {str(round(y,3))}')
 
 sqt1()
