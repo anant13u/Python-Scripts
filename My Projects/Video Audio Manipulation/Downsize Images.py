ffddf97b661437da39692706c392b49a3b6bc0dc
Anant <anant13u@gmail.com>
2024-10-21 21:15:21 +0530

    Added boilerplate code for PySimpleGUI.

--- My Projects/Video Audio Manipulation/Downsize Images.py
+++ My Projects/Video Audio Manipulation/Downsize Images.py
@@ -1,1 +1,42 @@
-Creating new script to downsize image(s).
+import os
+from pathlib import Path
+import PySimpleGUI as sg
+from datetime import datetime
+import subprocess
+
+sg.theme('Reddit')
+# sg.theme_previewer()
+
+selectFolderText = sg.Text('Select Folder',s=(30,2),pad=((40,20),10))
+folderBrowse = sg.FolderBrowse(key='-basepath-',s=(15,2),pad=(40,10))
+
+layout = [  [selectFolderText, folderBrowse],
+            [sg.B('Generate List',s=(15,2),pad=(30,10)), sg.B('Exit',s=(15,2),pad=(70,10))]  ]
+
+Window = sg.Window('Generate list of files', layout)
+
+while True:
+    event, values = Window.read()
+    basepath = Path(values['-basepath-'])
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+    elif values['-basepath-']=='':
+        sg.popup('Please select a folder to perform operations in.')
+    elif event == 'Generate List':
+        subprocess.Popen(['explorer.exe', basepath]) # Below 2 lines give the same result.
+        # subprocess.run(['explorer', basepath])
+        # subprocess.Popen(['explorer', basepath])
+        print(basepath)
+
+
+
+
+# filelist = '\n'.join(filelist)
+            
+# current_file_size = round(os.path.getsize(current_file)/(1024*1024),2)
+
+# Window.disappear()
+# Window.reappear()
+        
+
+
