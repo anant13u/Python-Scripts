0cb43eb6f1086bf7b7a71d9b65dcfa60698a6725
Anant Upadhyay <anant13u@gmail.com>
2025-02-04 11:14:37 +0530

    Added validation to ensure the number of new filenames matches the original list before renaming. Included a 'hyphen' event to copy formatted text to the clipboard.

--- My Projects/File Renamers/MultilineMassFileRenamer (GUI).py
+++ My Projects/File Renamers/MultilineMassFileRenamer (GUI).py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import PySimpleGUI as sg
 from datetime import datetime
+import pyperclip
 
 rename_log = []
 
@@ -11,7 +12,7 @@ selectFolderText = sg.Text('Select Folder', s=(80), p=((290,20),10))
 folderBrowse = sg.FolderBrowse(k='-basepath-', s=(15,2), p=(30,10))
 
 layout = [  [selectFolderText, folderBrowse], 
-            [sg.B('Generate List', s=(15,2), p=((350,120),10)), sg.B('Rename Files', s=(15,2), p=(10,10)), sg.B('Exit', s=(15,2), p=(120,10))], 
+            [sg.B('Generate List', s=(15,2), p=((250,120),10)), sg.B('Rename Files', s=(15,2), p=(10,10)), sg.B('Exit', s=(15,2), p=(120,10)), sg.B('Copy -', k='hyphen', s=(15,2))], 
             [sg.Multiline('', k='file_list', s=(80,13), p=(50,20), font=("Bahnschrift", 21))], 
             [sg.T('Script Creator: Anant Upadhyay')]  ]
 
@@ -47,44 +48,49 @@ while True:
         curr_names = os.listdir(basepath)
         # Window['file_list'].update('\n'.join(curr_names), visible=True)
         Window['file_list'].update('\n'.join(curr_names))
+    elif event == 'hyphen':
+        pyperclip.copy('  -  ')
     elif event == 'Rename Files':
         if values['file_list'] == '':
             sg.popup('Please generate the file list first.', keep_on_top=True)
         else:
             # Get new names from GUI and create rename_dict
             new_names = values['file_list'].split('\n')
-            rename_dict = dict(zip(curr_names, new_names))
+            if len(new_names) > len(curr_names) or len(new_names) < len(curr_names):
+                sg.popup('Number of new filenames does not match number of filenames from original list.', keep_on_top=True)
+            else:
+                rename_dict = dict(zip(curr_names, new_names))
 
-            # Populate rename_log with renaming information
-            for curr_name, new_name in rename_dict.items():
-                if curr_name != new_name:
-                    rename_log.append(f'{curr_name}\n{new_name}\n')
-            if len(rename_log) == 0: # Check if there are files to be renamed
-                sg.popup('No files to be renamed.', keep_on_top=True)
-            elif len(rename_log) == 1: # Check if there is exactly one file to be renamed
-                # Display a popup with the first renaming operation and ask for final confirmation
-                if sg.popup_yes_no(f'Please check new names before renaming:\n{rename_log[0]}', title='Final Check', keep_on_top=True, line_width=200) == 'Yes':
-                    rename_files()
-            # If there are multiple files to be renamed, join them with newline for better readability
-            elif len(rename_log) > 1:
-                rename_log = '\n'.join(rename_log)
-                popup_layout = [    [sg.Multiline(f'Please check new names before renaming:\n{rename_log}', s=(60,20))], 
-                                    [sg.B('Yes'), sg.B('No')]  ]
-                popup_window = sg.Window('Final Check', popup_layout, keep_on_top=True)
-                # Ask for final confirmation before proceeding with renaming
-                while True:
-                    popup_event, popup_values = popup_window.read()
-                    if popup_event in (sg.WINDOW_CLOSED, 'No'):
-                        break
-                    elif popup_event == 'Yes':
+                # Populate rename_log with renaming information
+                for curr_name, new_name in rename_dict.items():
+                    if curr_name != new_name:
+                        rename_log.append(f'{curr_name}\n{new_name}\n')
+                if len(rename_log) == 0: # Check if there are files to be renamed
+                    sg.popup('No files to be renamed.', keep_on_top=True)
+                elif len(rename_log) == 1: # Check if there is exactly one file to be renamed
+                    # Display a popup with the first renaming operation and ask for final confirmation
+                    if sg.popup_yes_no(f'Please check new names before renaming:\n{rename_log[0]}', title='Final Check', keep_on_top=True, line_width=200) == 'Yes':
                         rename_files()
-                        break
-                popup_window.close()
-            # Reset rename_log and update GUI
-            rename_log = []
-            curr_names = os.listdir(basepath)
+                # If there are multiple files to be renamed, join them with newline for better readability
+                elif len(rename_log) > 1:
+                    rename_log = '\n'.join(rename_log)
+                    popup_layout = [    [sg.Multiline(f'Please check new names before renaming:\n{rename_log}', s=(60,20))], 
+                                        [sg.B('Yes'), sg.B('No')]  ]
+                    popup_window = sg.Window('Final Check', popup_layout, keep_on_top=True)
+                    # Ask for final confirmation before proceeding with renaming
+                    while True:
+                        popup_event, popup_values = popup_window.read()
+                        if popup_event in (sg.WINDOW_CLOSED, 'No'):
+                            break
+                        elif popup_event == 'Yes':
+                            rename_files()
+                            break
+                    popup_window.close()
+                # Reset rename_log and update GUI
+                rename_log = []
+                curr_names = os.listdir(basepath)
 
-            Window['file_list'].update('\n'.join(curr_names))
+                Window['file_list'].update('\n'.join(curr_names))
 
 
 
